01af541e363b6acba911475da5ccc32ee2f5cf7e
Stefan Cutovic <166257381+SCuto26@users.noreply.github.com>
2025-08-27 23:29:58 -0700

    Updated sayhello.c addressing formatting challenge + additional comments

--- homework/homework01/sayhello.c
+++ homework/homework01/sayhello.c
@@ -1,10 +1,17 @@
+// Program that prompts user for their name and greets them
+// Contributors: Cutovic, Jackson, Smith
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main( int argc, char * argv[] ) {
+int main(int argc, char * argv[]) {
    char input[25];
-   printf( "What is your name: " );
+   
+   printf("What is your name: ");
    scanf("%s", input);
-   printf("\n Hello, %s \n", input);
+   
+   // Extra blank lines and indentation for formatting challenge
+   printf("\n\n    Hello, %s!\n\n", input);
+   
    exit(0);
 }

c76fc1b73d9d4c39705ed6f9e87bcb35ea8d4640
asmith2828 <asmit217@lion.lmu.edu>
2025-08-27 19:01:40 -0700

    Add files via upload

--- homework/homework01/sayhello.c
+++ homework/homework01/sayhello.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main( int argc, char * argv[] ) {
-
-   printf( "\n\n   Hello, world!\n\n" );
-
+   char input[25];
+   printf( "What is your name: " );
+   scanf("%s", input);
+   printf("\n Hello, %s \n", input);
+   exit(0);
 }
